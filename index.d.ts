a48f6a5f3dc03f34fee93ff8cee579e5b55d812f
pavekovt <39086668+pavekovt@users.noreply.github.com>
2020-06-27 14:24:28 +0300

    Update index.d.ts

--- index.d.ts
+++ index.d.ts
@@ -1400,7 +1400,7 @@ interface BrowserMp {
 	markAsChat(): void;
 	reload(ignoreCache: boolean): void;
 	call(eventName: string, ...args: any[]): void;
-	callProc(procName: string, args?: any[]): Promise<any>;
+	callProc(procName: string, ...args: any[]): Promise<any>;
 	executeCached(code: string): void;
 }
 
@@ -3326,8 +3326,8 @@ interface EventMpPool {
 	call(eventName: string, ...args: any[]): void;
 	callRemoteUnreliable(eventName: string, ...args: any[]): void;
 	callRemote(eventName: string, ...args: any[]): void;
-	callRemoteProc(procName: string, args?: any[]): Promise<any>;
-	callProc(procName: string, args?: any[]): Promise<any>;
+	callRemoteProc(procName: string, ...args: any[]): Promise<any>;
+	callProc(procName: string, ...args: any[]): Promise<any>;
 	cancelPendingRpc(procName: string): void;
 	hasPendingRpc(procName: string): boolean;
 	remove(eventName: string, handler?: (...args: any[]) => void): void;
